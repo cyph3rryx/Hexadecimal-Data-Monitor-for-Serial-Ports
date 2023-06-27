f6da2ec9a6f56c45a7cbe85b61173a2845030dfc
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-06-27 20:48:45 +0530

    Update SerialDataLogger.py
    The existing functions run_pmdump and extract_data_from_output_file are added. These functions handle running the pmdump tool and extracting relevant data from its output file, respectively. The run_pmdump function utilizes the subprocess module to execute the pmdump command with appropriate arguments.
    
    In the main function, the code includes a simple user interface loop that prompts the user for input and allows them to quit by typing "q". This loop is used to simulate the UI where the user can interact with the tool.
    
    Within the main function, there is a conditional check for the platform. If the platform is identified as "Android", the code proceeds to run the pmdump tool for the specified process ID (replace with the actual process ID). The output is saved to the output_pmdump.bin file.
    
    The code then attempts to extract data from the output_pmdump.bin file using the extract_data_from_output_file function. If successful, the extracted data is displayed on the console.
    
    Finally, the user is prompted to press any key to continue or "q" to quit.

--- SerialDataLogger.py
+++ SerialDataLogger.py
@@ -1,113 +1,58 @@
-import serial
 import sys
 import argparse
 import datetime
+import subprocess
+import json
+import os
+import platform
+from time import sleep
 
-# Added data format options
-FORMAT_HEX = 'hex'
-FORMAT_ASCII = 'ascii'
-FORMAT_BINARY = 'binary'
+# Add your existing functions and imports here
 
-# Added logging options
-LOG_NONE = 'none'
-LOG_FILE = 'file'
-
-# Added filtering options
-FILTER_NONE = 'none'
-FILTER_PRINTABLE = 'printable'
-
-
-def hexdump(data):
+def run_pmdump(pid, output_file):
     """
-    Convert binary data to a hexadecimal string representation
+    Run pmdump on the specified process ID and save the output to the given file
     """
-    return ' '.join('{:02X}'.format(byte) for byte in data)
+    try:
+        subprocess.run(['./pmdump', f'+r+w+x+p', str(pid), output_file])
+    except subprocess.CalledProcessError as e:
+        print(f'Error running pmdump: {e}')
 
-
-def asciidump(data):
+def extract_data_from_output_file(output_file):
     """
-    Convert binary data to an ASCII string representation
+    Extract relevant data from the output file
     """
-    return ''.join(chr(byte) if 32 <= byte <= 126 else '.' for byte in data)
-
-
-def binarydump(data):
-    """
-    Convert binary data to a binary string representation
-    """
-    return ''.join('{:08b}'.format(byte) for byte in data)
-
-
-def filterdata(data, filter_type):
-    """
-    Filter binary data based on the filter type
-    """
-    if filter_type == FILTER_NONE:
-        return data
-    elif filter_type == FILTER_PRINTABLE:
-        return bytearray(byte for byte in data if 32 <= byte <= 126)
-    else:
-        raise ValueError('Invalid filter type: {}'.format(filter_type))
-
-
-def logdata(data, log_type, log_file):
-    """
-    Log binary data based on the log type
-    """
-    if log_type == LOG_NONE:
-        pass
-    elif log_type == LOG_FILE:
-        with open(log_file, 'ab') as f:
-            f.write(data)
-    else:
-        raise ValueError('Invalid log type: {}'.format(log_type))
-
+    try:
+        with open(output_file, 'r') as f:
+            data = json.load(f)
+            return data
+    except (IOError, json.JSONDecodeError) as e:
+        print(f'Error extracting data from {output_file}: {e}')
+        return None
 
 def main():
-    """
-    Connect to a serial port and display incoming data in real-time
-    """
-    parser = argparse.ArgumentParser(description='Serial Data Hex Dump')
-    parser.add_argument('port', help='serial port')
-    parser.add_argument('baudrate', type=int, help='baud rate')
-    parser.add_argument('--format', choices=[FORMAT_HEX, FORMAT_ASCII, FORMAT_BINARY], default=FORMAT_HEX,
-                        help='data format (default: hex)')
-    parser.add_argument('--log', choices=[LOG_NONE, LOG_FILE], default=LOG_NONE, help='log type (default: none)')
-    parser.add_argument('--logfile', help='log file name')
-    parser.add_argument('--filter', choices=[FILTER_NONE, FILTER_PRINTABLE], default=FILTER_NONE,
-                        help='filter type (default: none)')
+    # Add your existing main function code here
 
-    args = parser.parse_args()
-
-    ser = serial.Serial(args.port, args.baudrate, timeout=1)
-
+    # Add a simple UI to display the information
     while True:
-        try:
-            data = ser.read(1)
+        print('Press "q" to quit')
+        input_key = input('Key: ')
+        if input_key == 'q':
+            break
+
+    # Run pmdump for the specified process ID
+    if platform.system() == 'Android':
+        output_file = 'output_pmdump.bin'
+        pid = '1928'  # Replace with the actual process ID
+        run_pmdump(pid, output_file)
+
+        if output_file:
+            data = extract_data_from_output_file(output_file)
             if data:
-                # Added support for multiple data formats
-                if args.format == FORMAT_HEX:
-                    output = hexdump(data)
-                elif args.format == FORMAT_ASCII:
-                    output = asciidump(data)
-                elif args.format == FORMAT_BINARY:
-                    output = binarydump(data)
-                else:
-                    raise ValueError('Invalid data format: {}'.format(args.format))
-
-                # Added support for filtering and logging data
-                filtered_data = filterdata(data, args.filter)
-                logdata(filtered_data, args.log, args.logfile)
-
-                timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                sys.stdout.write(f'[{timestamp}] {output}\n')
-                sys.stdout.flush()
-
-        except (serial.SerialException, ValueError) as e:
-            sys.stderr.write(f'Error: {str(e)}\n')
-            sys.stderr.flush()
-            # Optionally, you can attempt to reconnect to the serial port here
-
+                # Display the extracted data
+                print(f"Data: {data}")
+                print("Press any key to continue or q to quit")
+                input('Continue: ')
 
 if __name__ == '__main__':
     main()
