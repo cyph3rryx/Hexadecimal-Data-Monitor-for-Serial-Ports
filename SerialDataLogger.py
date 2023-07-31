1ad4da9322276e511aa78b7a25a362ed5ab0af4e
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-08-01 00:38:34 +0530

    Update SerialDataLogger.py

--- SerialDataLogger.py
+++ SerialDataLogger.py
@@ -2,32 +2,45 @@ import sys
 import argparse
 import datetime
 import subprocess
-import json
 import os
 import platform
 import hashlib
 import time
+import logging
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
 
-def run_pmdump(pid, output_file):
-    """
-    Run pmdump on the specified process ID and save the output to the given file
-    """
-    try:
-        subprocess.run(['./pmdump', f'+r+w+x+p', str(pid), output_file])
-    except subprocess.CalledProcessError as e:
-        print(f'Error running pmdump: {e}')
+# Add your preferred file analysis tool import here 
 
-def extract_data_from_output_file(output_file):
+# Set up the logger
+logging.basicConfig(filename='memory_analysis.log', level=logging.INFO)
+
+def run_cmd(command):
     """
-    Extract relevant data from the output file
+    Runs a shell command and returns the output
     """
-    try:
-        with open(output_file, 'r') as f:
-            data = json.load(f)
-            return data
-    except (IOError, json.JSONDecodeError) as e:
-        print(f'Error extracting data from {output_file}: {e}')
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    if process.returncode != 0:
+        logging.error(f'Error running command: {command}\nError: {error.decode()}')
         return None
+    return output.decode()
+
+def run_volatility(file_path, command):
+    """
+    Run Volatility on the specified file with the given command return the output
+    """
+    vol_command = f'vol.py -f {file_path} {command}'
+    return run_cmd(vol_command)
+
+def file_analysis(file_path):
+    """
+    Run file analysis tool on given file and return the output
+    """
+    # Replace 'file_analysis_tool' with your actual tool's command
+    file_analysis_command = f'file_analysis_tool {file_path}'
+    return run_cmd(file_analysis_command)
 
 def calculate_hash(file_path):
     """
@@ -39,39 +52,90 @@ def calculate_hash(file_path):
             hash_value = hashlib.sha256(data).hexdigest()
             return hash_value
     except IOError as e:
-        print(f'Error calculating hash: {e}')
+        logging.error(f'Error calculating hash: {e}')
         return None
 
-def main():
-    # Add your existing main function code here
+# Add your anomaly detection function here
+def anomaly_detection(data):
+    """
+    Detect anomalies in a list of numbers using the Z-score method
+    """
+    # Convert the data to a numpy array
+    data = np.array(data)
 
-    # Add a simple UI to display the information
+    # Calculate the mean and standard deviation of the data
+    mean = np.mean(data)
+    std_dev = np.std(data)
+
+    # Calculate the Z-scores of the data
+    z_scores = (data - mean) / std_dev
+
+    # Define a threshold for what constitutes an anomaly
+    # In this case, a data point is considered an anomaly if its Z-score is more than 3 standard deviations away from the mean
+    threshold = 3
+
+    # Find the indices of the anomalies in the data
+    anomalies = np.where(np.abs(z_scores) > threshold)
+
+    # Return the anomalies
+    return data[anomalies]
+
+# Add your timeline analysis function here
+def timeline_analysis(events):
+    """
+    Perform timeline analysis on a list of events and plot the results
+    """
+    # Sort the events by timestamp
+    events.sort(key=lambda x: x['timestamp'])
+
+    # Extract the timestamps and data from the events
+    timestamps = [event['timestamp'] for event in events]
+    data = [event['data'] for event in events]
+
+    # Convert the timestamps to datetime objects
+    timestamps = [datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S') for timestamp in timestamps]
+
+    # Plot the data
+    plt.plot(timestamps, data)
+    plt.xlabel('Time')
+    plt.ylabel('Data')
+    plt.title('Timeline Analysis')
+    plt.show()
+
+def main():
+    print('Press "q" to quit at any time')
+
     while True:
-        print('Press "q" to quit')
-        print('1. RAM Dump')
+        print('1. RAM Dump and Analysis')
         print('2. Study Page File')
         choice = input('Enter your choice: ')
 
         if choice == '1':
-            print('Initiating RAM Dump...')
-            output_file = 'output_pmdump.bin'
-            pid = '1928'  # Replace with the actual process ID
-            run_pmdump(pid, output_file)
+            print('Initiating RAM Dump and Analysis...')
+            file_path = 'memory_dump.bin'  # Replace with the actual file path
 
-            if output_file:
-                data = extract_data_from_output_file(output_file)
-                if data:
-                    # Display the extracted data
-                    print(f"Data: {data}")
-                    print("Press any key to continue or q to quit")
-                    input('Continue: ')
+            # Run the volatility info command 
+            output = run_volatility(file_path, 'info')
 
-                    # Calculate hash value
-                    hash_value = calculate_hash(output_file)
-                    if hash_value:
-                        print(f"Hash Value: {hash_value}")
-                    else:
-                        print("Error calculating hash value")
+            if output:
+                # Display the output
+                print(f"Output: {output}")
+
+                # Calculate hash value
+                hash_value = calculate_hash(file_path)
+                if hash_value:
+                    print(f"Hash Value: {hash_value}")
+                else:
+                    print("Error calculating hash value")
+            
+            # File analysis with external tool 
+            analysis_output = file_analysis(file_path)
+            if analysis_output:
+                print(f"File Analysis output: {analysis_output}")
+
+            # Replace 'anomaly_detection' and 'timeline_analysis' with your actual functions
+            print(f"Anomaly detection results: {anomaly_detection(output)}")
+            print(f"Timeline analysis result: {timeline_analysis(output)}")
 
         elif choice == '2':
             print('Studying Page File...')
