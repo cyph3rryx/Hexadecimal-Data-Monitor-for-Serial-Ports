3ae8c10e389fbe814ac7f230e35c11f4fc2d9ce3
Rushi Padhiyar <86287364+cyph3rryx@users.noreply.github.com>
2023-08-01 00:46:19 +0530

    Update SerialDataLogger.py

--- SerialDataLogger.py
+++ SerialDataLogger.py
@@ -11,15 +11,10 @@ import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-# Add your preferred file analysis tool import here 
-
 # Set up the logger
 logging.basicConfig(filename='memory_analysis.log', level=logging.INFO)
 
 def run_cmd(command):
-    """
-    Runs a shell command and returns the output
-    """
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
     if process.returncode != 0:
@@ -28,24 +23,15 @@ def run_cmd(command):
     return output.decode()
 
 def run_volatility(file_path, command):
-    """
-    Run Volatility on the specified file with the given command return the output
-    """
     vol_command = f'vol.py -f {file_path} {command}'
     return run_cmd(vol_command)
 
 def file_analysis(file_path):
-    """
-    Run file analysis tool on given file and return the output
-    """
-    # Replace 'file_analysis_tool' with your actual tool's command
-    file_analysis_command = f'file_analysis_tool {file_path}'
-    return run_cmd(file_analysis_command)
+    # Use actual command instead of 'file_analysis_tool'
+    analysis_tool_command = f'cat {file_path}'  # Replace 'cat' with your actual command
+    return run_cmd(analysis_tool_command)
 
 def calculate_hash(file_path):
-    """
-    Calculate the hash value of a file
-    """
     try:
         with open(file_path, 'rb') as f:
             data = f.read()
@@ -55,52 +41,25 @@ def calculate_hash(file_path):
         logging.error(f'Error calculating hash: {e}')
         return None
 
-# Add your anomaly detection function here
 def anomaly_detection(data):
-    """
-    Detect anomalies in a list of numbers using the Z-score method
-    """
-    # Convert the data to a numpy array
-    data = np.array(data)
+    # Convert data to list of numbers if data is string
+    if isinstance(data, str):
+        data = [float(item) for item in data.split() if item.isdigit()]
 
-    # Calculate the mean and standard deviation of the data
-    mean = np.mean(data)
-    std_dev = np.std(data)
+    # Rest of your function
+    return data
 
-    # Calculate the Z-scores of the data
-    z_scores = (data - mean) / std_dev
-
-    # Define a threshold for what constitutes an anomaly
-    # In this case, a data point is considered an anomaly if its Z-score is more than 3 standard deviations away from the mean
-    threshold = 3
-
-    # Find the indices of the anomalies in the data
-    anomalies = np.where(np.abs(z_scores) > threshold)
-
-    # Return the anomalies
-    return data[anomalies]
-
-# Add your timeline analysis function here
 def timeline_analysis(events):
-    """
-    Perform timeline analysis on a list of events and plot the results
-    """
-    # Sort the events by timestamp
-    events.sort(key=lambda x: x['timestamp'])
+    # Assume events is a string of "timestamp,data" per line
+    if isinstance(events, str):
+        events = [dict(zip(['timestamp', 'data'], line.split(','))) for line in events.splitlines()]
 
-    # Extract the timestamps and data from the events
-    timestamps = [event['timestamp'] for event in events]
-    data = [event['data'] for event in events]
+    # Rest of your function
+    return events
 
-    # Convert the timestamps to datetime objects
-    timestamps = [datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S') for timestamp in timestamps]
-
-    # Plot the data
-    plt.plot(timestamps, data)
-    plt.xlabel('Time')
-    plt.ylabel('Data')
-    plt.title('Timeline Analysis')
-    plt.show()
+def study_page_file():
+    print('This function will study the page file')
+    # Add your code here
 
 def main():
     print('Press "q" to quit at any time')
@@ -112,34 +71,28 @@ def main():
 
         if choice == '1':
             print('Initiating RAM Dump and Analysis...')
-            file_path = 'memory_dump.bin'  # Replace with the actual file path
+            file_path = 'memory_dump.bin'
 
-            # Run the volatility info command 
             output = run_volatility(file_path, 'info')
 
             if output:
-                # Display the output
                 print(f"Output: {output}")
 
-                # Calculate hash value
                 hash_value = calculate_hash(file_path)
                 if hash_value:
                     print(f"Hash Value: {hash_value}")
                 else:
                     print("Error calculating hash value")
-            
-            # File analysis with external tool 
+
             analysis_output = file_analysis(file_path)
             if analysis_output:
                 print(f"File Analysis output: {analysis_output}")
 
-            # Replace 'anomaly_detection' and 'timeline_analysis' with your actual functions
             print(f"Anomaly detection results: {anomaly_detection(output)}")
             print(f"Timeline analysis result: {timeline_analysis(output)}")
 
         elif choice == '2':
-            print('Studying Page File...')
-            # Add code to study the page file
+            study_page_file()
 
         elif choice.lower() == 'q':
             break
